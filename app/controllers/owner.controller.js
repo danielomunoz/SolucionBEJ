3dda4d91ba56036bbf55fae8707b5faf46b77d79
danielomunoz <danielmunozamaya1@gmail.com>
2020-09-24 13:15:33 +0200

    Mision 2 - Reto A

--- app/controllers/owner.controller.js
+++ app/controllers/owner.controller.js
@@ -24,13 +24,33 @@ exports.findAll = async (req, res, next) => {
       throw createError(400, 'Bad requests. Params are needed in their correct format', validation_errors.array());
     }
 
-    // Reading page
-    let page = Number(req.query.page) || cnsts.controllers.owner.MIN_OFFSET_REQUESTS;
+    // Creating pagination values.
+    let offset_requests = Number(req.query.page) || cnsts.controllers.owner.MIN_OFFSET_REQUESTS;
+    let n_requests = Number(req.query.limit / cnsts.controllers.owner.N_REQUESTS_FACTOR) || cnsts.controllers.owner.MIN_N_REQUESTS;
+    let firs_page_to_search = (n_requests*offset_requests - n_requests + cnsts.controllers.owner.ITERATION_ARRAY_FACTOR);
+    let last_page_to_search = (n_requests*offset_requests);
 
-    let	owners_info = await getOwners(page);
+    // Creating response object.
+    let response = { code: cnsts.http_codes.OK, meta: { pagination: { total: 0, pages: 0, page: Number(req.query.page), limit: Number(req.query.limit)} }, data: [] };
+
+    let owners_info;
+
+    // Doing some recurring requests filling our owners_info object, because the pagination of the external
+    // api of users and our own don't match.
+    for(let page = firs_page_to_search; page <= last_page_to_search; page++){
+        owners_info = await getOwners(page);
+        
+        if(!isEmptyArray(owners_info.data)) response.data.push(owners_info.data);
+
+        // Below, I ask for the last operation of the loop to finally calculate the total owners and the pages of the response object
+        if(page == last_page_to_search){
+            response.meta.pagination.total = owners_info.meta.pagination.total;
+            response.meta.pagination.pages = Math.floor(owners_info.meta.pagination.pages/n_requests);
+        }
+    }
 
     // Send complete response, according to our own pagination.
-    res.status(200).json(owners_info);
+    res.status(200).json(response);
 
 	} catch(err) { next(err); }
 
